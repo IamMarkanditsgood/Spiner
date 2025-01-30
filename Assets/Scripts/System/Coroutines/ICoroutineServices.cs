1652789b5ab73553fdf5f15fb12a6cd07b9c9ec6
PavloMarkovskiy <IamMarkanditsgood@users.noreply.github.com>
2025-01-30 00:05:01 +0000

    Cleaned unusable system

--- Assets/Scripts/System/Coroutines/ICoroutineServices.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using UnityEngine;
-
-public interface ICoroutineServices 
-{
-    Coroutine StartRoutine(IEnumerator enumerator);
-
-    void StopRoutine(Coroutine routine);
-}
